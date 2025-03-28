2fc9563d5d6dbea276a2bcbec6af9eed8d3b0855
Антон <akiosaito550@gmail.com>
2025-03-28 18:53:12 +0300

    added classes Product, User

---
+++ TheForgingDwarf/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheForgingDwarf
+{
+    class Product
+    {
+        int Id { get; set; }
+        public string Name { get; set; }
+        public int TypeCode { get; set; }
+        public int StyleCode { get; set; }
+        public int SteelCode { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ TheForgingDwarf/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheForgingDwarf
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int RaceCode { get; set; }
+
+        public bool IsSuperUser { get; set; }
+    }
+}
