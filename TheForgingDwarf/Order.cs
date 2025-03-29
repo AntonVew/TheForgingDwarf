6e194691463a562fcc2ddf29164615b8a6760ec0
Антон <akiosaito550@gmail.com>
2025-03-29 18:19:12 +0300

    added class Order

---
+++ TheForgingDwarf/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheForgingDwarf
+{
+    class Order
+    {
+        public int Number { get; set; }
+        public DateTime DateStart { get; set; }
+        public int StatusCode { get; set; }
+        public int ClientID { get; set; }
+        public bool IsCustom { get; set; }
+        public string Name { get; set; }
+        public int TypeCode { get; set; }
+        public int StyleCode { get; set; }
+        public int SteelCode { get; set; }
+        public decimal Price { get; set; }
+    }
+}
